a21575e76d82045baaa98f75be75b9e2a3438f19
Adina Țeudan <adina.teudan@gmail.com>
2016-05-21 16:02:01 +0300

    Added import from csv for lines.

--- hacktm2016/importer.py
+++ hacktm2016/importer.py
@@ -1,7 +1,8 @@
 from typing import List
 
-from ratt import Station
+from ratt import Station, Line
 import csv
+import re
 
 
 def parse_stations_from_csv(filename: str) -> List[Station]:
@@ -25,4 +26,18 @@ def parse_stations_from_csv(filename: str) -> List[Station]:
 				continue
 
 	return list(result)
+
+
+def parse_lines_from_csv(filename: str) -> List[Line]:
+	with open(filename, newline='') as csvfile:
+		result = []
+
+		filereader = csv.DictReader(csvfile, delimiter=',', quotechar ='"')
+
+		for row in filereader:
+
+			line = Line(row['LineID'], row['LineName'], row['LineType'], row['RouteName1'], row['RouteName2'])
+			result.append(line)
+
+		return result
 
